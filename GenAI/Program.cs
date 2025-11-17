c926f1f2afc5a3b1ef3c01f8c5b0d68793e3f005
Shiraz Adam <93381720+ShirazAdam@users.noreply.github.com>
2025-11-17 23:58:50 +0000

    using Phi-4 model and GPU

--- GenAI/Program.cs
+++ GenAI/Program.cs
@@ -1,10 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 
 using Microsoft.ML.OnnxRuntimeGenAI;
 
-// The absolute path to the folder where the Phi-3 model is stored (folder to the '.onnx' file)
-const string modelPath = @"D:\onnx\cpu_and_mobile\cpu-int4-rtn-block-32";
-var model = new Model(modelPath);
+// The absolute path to the folder where the Phi-4 model is stored (folder to the '.onnx' file)
+// using https://huggingface.co/microsoft/Phi-4-mini-instruct-onnx
+
+const string modelPath = @"D:\phi4mini\gpu\gpu-int4-rtn-block-32";
+using var config = new Config(modelPath);
+config.ClearProviders();
+
+#if DEBUG_CUDA
+config.SetProviderOption("cuda", "enable_cuda_graph", "0");
+#endif
+
+using var model = new Model(config);
 var tokenizer = new Tokenizer(model);
 
 const string initialPrompt = "Answer the following question as clearly and concisely as possible, providing any relevant information and examples.";
@@ -44,7 +53,7 @@ do
 
     Console.WriteLine(
         $"Streaming Tokens: {totalTokens} Time: {runTimeInSeconds:0.00} Tokens per second: {totalTokens / runTimeInSeconds:0.00}");
-    
+
     Console.WriteLine();
     Console.WriteLine("Next prompt:");
 
